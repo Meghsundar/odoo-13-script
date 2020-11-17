f42d368de2fd7d09a3cd7b5d122cdfeef04d69e2
Meghsundar <52901562+Meghsundar@users.noreply.github.com>
2020-11-17 17:58:05 +0530

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -78,7 +78,7 @@ echo -e "\n--- Installing Python 3 + pip3 --"
 sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 libjpeg-dev gdebi -y
 
 echo -e "\n---- Install python packages/requirements ----"
-sudo -H pip3 install -r https://gist.github.com/Meghsundar/3ca14f717471e4a125b7696f08b9a714.js
+sudo -H pip3 install -r https://raw.githubusercontent.com/Meghsundar/odoo-13-script/main/requirements.txt
 
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
 sudo apt-get install nodejs npm -y
